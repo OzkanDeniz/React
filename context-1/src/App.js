cd0d4a4415e14a003dc6029f1df092d35872fb86
ozkan <ozkandnz09@gmail.com>
2024-09-11 20:28:53 +0300

    changecolor

--- context-1/src/App.js
+++ context-1/src/App.js
@@ -11,15 +11,21 @@ export const StudentContext=createContext()
 const App = () => {
 const[students,setStudents]= useState(data)
 
+const changeColor = (id,newRenk) =>{
+setStudents(students.map((a)=>a.id===id ? {...a, color: newRenk} : a))
 
+}
 
 
   return (
+    //!  2 bütün projeye gönderilmek üzere ilk Home sayfasını (gönderilecek verilerle) sarmalladım
     <div>
 
 
-      
+      <StudentContext.Provider value={{students,changeColor}}>
       <Home/>
+      </StudentContext.Provider>
+      
 
     </div>
   )
--- context-1/src/components/OgrenciItem.jsx
+++ context-1/src/components/OgrenciItem.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { StudentContext } from '../App'
 
 const OgrenciItem = () => {
+
+//!3 (context consumer) globalde  (mesela App.js yada ayrı bir context sayfası) tanımladığım değerleri buraya çağırdık
+
+
+const {students} = useContext(StudentContext)
+
+
+
+
   return (
-    <div>OgrenciItem</div>
+    <div>
+      {students.map((std)=>(
+        <div key={std.id}  style={{background:std.color}}>
+          <h3>NAME: {std.name}</h3>
+          <h4>EMAİL :{std.mail}</h4>
+          <h4>AGE: {std.age}</h4>
+
+Color: <input type="text" value={std.color}
+onChange={(e)=>changeColor(std.id,e.target.value)}
+
+/>
+
+        </div>
+      ))}
+    </div>
   )
 }
 
