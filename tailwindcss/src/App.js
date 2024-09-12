699dc29d80db4ad6da688b89046d8aa71a4cb747
ozkan <ozkandnz09@gmail.com>
2024-09-12 20:34:52 +0300

    margin padding

--- tailwindcss/src/App.js
+++ tailwindcss/src/App.js
@@ -1,10 +1,12 @@
+import SpacingandSizing from "./components/SpacingandSizing";
 import Typography from "./components/Typography";
 
 function App() {
   return (
     <div className="App">
      <h1 className="text-2xl text-center text-purple-700 font-bold">Merhaba Tailwind</h1>
-     <Typography/>
+     {/* <Typography/> */}
+     <SpacingandSizing/>
     </div>
   );
 }
---
+++ tailwindcss/src/components/SpacingandSizing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SpacingandSizing = () => {
+  return (
+    <div>
+      <h3 className='text-2xl'>Spacing and Sizing</h3>
+      <button className='bg-blue-500 text-white p-4 m-1'>Tıkla</button>
+      <button className='bg-blue-500 text-white px-4'>Tıkla</button>
+      <button className='bg-blue-500 text-white ms-4'>Tıkla</button>
+      <button className='bg-blue-500 text-white ml-4'>Tıkla</button>
+      <button className='bg-blue-500 text-white px-4 py-4 ml-2'>Tıkla</button>
+    </div>
+  )
+}
+
+export default SpacingandSizing
