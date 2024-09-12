e0180e226fecec7d7a7d2da0921faeabd7482a4b
ozkan <ozkandnz09@gmail.com>
2024-09-12 19:52:28 +0300

    tailwindcssbegining

---
+++ tailwindcss/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-2xl text-center text-purple-700 font-bold">Merhaba Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
