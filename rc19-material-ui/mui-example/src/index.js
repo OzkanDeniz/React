9f4550ccd52537f07fc89f3ab132cc1eae2da5a0
ozkan <ozkandnz09@gmail.com>
2024-09-21 10:17:03 +0300

    material-ui

--- rc19-material-ui/mui-example/src/index.js
+++ rc19-material-ui/mui-example/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter } from "react-router-dom";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
