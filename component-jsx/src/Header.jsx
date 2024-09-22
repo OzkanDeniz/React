b4444ed1882c3d73af353765eed322c48bb75dcd
ozkan <ozkandnz09@gmail.com>
2024-09-22 12:50:32 +0300

    component

---
+++ component-jsx/src/Header.jsx
@@ -0,0 +1,14 @@
+
+
+
+const Header = () => {
+    const mesaj = "Hello World"
+  return (
+    <header>
+      <h1>{mesaj}</h1>
+      {/* yorum satırı */}
+    </header>
+  );
+};
+
+export default Header;
